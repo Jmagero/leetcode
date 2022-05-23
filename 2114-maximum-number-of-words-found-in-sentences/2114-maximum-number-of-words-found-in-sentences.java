d4956186335309b3213c9691f22426fc2c1c156b
Jocyline Magero <jmagero54@gmail.com>
2022-05-23 11:33:33 -0500

    Time: 8 ms (16.54%), Space: 46 MB (8.20%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
@@ -1,15 +1,9 @@
 class Solution {
     public int mostWordsFound(String[] sentences) {
-        //int[] arr = new int[sentences.length];
-        int max = 0;
+        int[] arr = new int[sentences.length];
         for(int i =0; i< sentences.length; i++){
-            max = Math.max(max,(sentences[i].split(" ").length));
-            //arr[i] = sentences[i].split(" ").length;
+            arr[i] = sentences[i].split(" ").length;
         }
-        // for( String sentence : sentences){
-        //     arr[sentence] = sentence.split(" ").length;
-        // }
-        //return Arrays.stream(arr).max().getAsInt();
-        return max;
+        return Arrays.stream(arr).max().getAsInt();
     }
 }
