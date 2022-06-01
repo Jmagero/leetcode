b4ca4d06e3889e32020c8171949e82f6fb2a832f
Jocyline Magero <jmagero54@gmail.com>
2022-06-01 08:59:35 -0500

    Time: 1 ms (97.44%), Space: 42 MB (99.67%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.java
+++ 1528-shuffle-string/1528-shuffle-string.java
@@ -4,8 +4,7 @@ class Solution {
         for(int i = 0; i < s.length(); i++){
            str[indices[i]] = s.charAt(i);
         }
-        return String.valueOf(str);
-        
+        return new String(str);
     }
 }
 
