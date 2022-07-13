1c1d35749015fe0487edf5c4e431bc8137de9a04
Jocyline Magero <jmagero54@gmail.com>
2022-07-13 08:54:57 -0500

    Time: 0 ms (100.00%), Space: 8.2 MB (70.19%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -3,23 +3,15 @@ public:
     void sortColors(vector<int>& nums) {
         int red = -1;
         int green = -1;
-        int blue = -1;
         for (int i = 0; i < nums.size(); i++){
             if(nums[i] == 0){
-              
-                // nums.set(red,nums.get(i));
-                // nums.set(i,nums.get(red));
                 green++;
                 swap(nums,green,i);
                 red++;
                 swap(nums,red,green);
-                // nums.set(green,nums.get(i));
-                // nums.set(i,nums.get(green));
             } else if(nums[i] == 1){
                 green++;
                 swap(nums,green,i);
-                // nums.set(green,nums.get(i));
-                // nums.set(i,nums.get(green));
             }
         }
     }
@@ -27,6 +19,5 @@ public:
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
-    }
-        
+    }   
 };
